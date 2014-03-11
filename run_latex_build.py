0f538772ad5126892012ab5439a2b391c795a3a8
Dominique Orban <dominique.orban@gmail.com>
2014-03-11 07:25:28 -0700

    Accept any number of build systems.

--- run_latex_build.py
+++ run_latex_build.py
@@ -2,7 +2,13 @@ import sublime_plugin
 
 
 class RunLatexBuildCommand(sublime_plugin.WindowCommand):
-    def run(self, build_system, reset_to):
-        self.window.run_command( "set_build_system", {"file": build_system } )
-        self.window.run_command( "build" )
-        self.window.run_command( "set_build_system", {"file": reset_to})
+
+  def run(self, build_systems=None):
+
+    if build_systems is None:
+      return
+    reset_to = build_systems[-1]
+    for build_system in build_systems[:-1]:
+      self.window.run_command( "set_build_system", {"file": build_system } )
+      self.window.run_command( "build" )
+    self.window.run_command( "set_build_system", {"file": reset_to})
